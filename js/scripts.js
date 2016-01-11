d892e66559c357efefe6e92c4a058a34ea60390f
Kathleen Graham and Kate Hall <kwarrenhall@gmail.com>
2016-01-11 13:18:11 -0800

    added newfile.html

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("h1").hover(function() {
+    alert("This is a header.");
+  });
+
+  $("h2").click(function() {
+  alert("This is a header.");
+  });
+
+  $("h3").click(function() {
+    alert("This is a header.");
+  });
+
+  $("li").dblclick(function() {
+    alert("This is a list.");
+  });
+
+});
